a28faaba7338f2f512bfb8bfa219baacfc3acd3e
Nikolay <merzlovnik@mail.ru>
2023-04-28 13:19:52 +0300

    Base AES settings

--- AES/Constants.cs
+++ AES/Constants.cs
@@ -2,5 +2,52 @@
 {
     public static class Constants
     {
+        /// <summary>
+        /// byte length in bits
+        /// </summary>
+        public static readonly int byteLength = 8;
+
+        public static readonly AESParameters aes128 = new(128, 10);
+
+        public static readonly AESParameters aes192 = new(192, 12);
+
+        public static readonly AESParameters aes256 = new(256, 14);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class AESParameters
+    {
+        /// <summary>
+        /// key length in bits
+        /// </summary>
+        public int KeyBits { get; private init; }
+
+        /// <summary>
+        /// rounds amount
+        /// </summary>
+        public int RoundsAmount { get; private init; }
+
+        /// <summary>
+        /// block lenght similar for every AES
+        /// </summary>
+        public static int BlockLength { get; }
+
+        public AESParameters(int keyBits, int roundsAmount)
+        {
+            KeyBits = keyBits;
+            RoundsAmount = roundsAmount;
+        }
+    }
+
+    /// <summary>
+    /// Available keys length for usage
+    /// </summary>
+    public enum AvailableKeysLength : int
+    {
+        key128 = 128,
+        key192 = 192,
+        key256 = 256
     }
 }
