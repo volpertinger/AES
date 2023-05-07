ec728441ac9ec252634e461344fbff35b23ae84a
Nikolay <merzlovnik@mail.ru>
2023-05-07 16:35:44 +0300

    GCD

--- AES/Utils.cs
+++ AES/Utils.cs
@@ -1,10 +1,37 @@
-﻿namespace AES
+﻿using System.Numerics;
+
+namespace AES
 {
     public static class Utils
     {
-        public static byte SumGF8(byte lhs, byte rhs)
+        /// <summary>
+        /// Finds the sum of lhs and rhs in Galua field 256
+        /// </summary>
+        public static byte SumGF256(byte lhs, byte rhs)
         {
             return (byte)(lhs ^ rhs);
+        }
+
+        /// <summary>
+        /// Euclidean algorithm
+        /// </summary>
+        /// <returns>GCD of lhs and rhs</returns>
+        public static byte GCD(byte lhs, byte rhs)
+        {
+            if (lhs == 0)
+                return rhs;
+            if (rhs == 0)
+                return lhs;
+            while (lhs != 0 && rhs != 0)
+            {
+                if (lhs > rhs)
+                    lhs %= rhs;
+                else
+                    rhs %= lhs;
+            }
+            if (lhs > rhs)
+                return lhs;
+            return rhs;
         }
     }
 }
--- AESTests/UtilsTests.cs
+++ AESTests/UtilsTests.cs
@@ -4,12 +4,43 @@ namespace AESTests
     public class UtilsTests
     {
         [TestMethod]
-        public void TestSumGF8()
+        public void TestSumGF256()
         {
-            Assert.AreEqual(0, AES.Utils.SumGF8(0, 0));
-            Assert.AreEqual(0, AES.Utils.SumGF8(1, 1));
-            Assert.AreEqual(0, AES.Utils.SumGF8(128, 128));
-            Assert.AreEqual(0, AES.Utils.SumGF8(255, 255));
+            // zeros
+            Assert.AreEqual(0, AES.Utils.SumGF256(0, 0));
+            Assert.AreEqual(0, AES.Utils.SumGF256(1, 1));
+            Assert.AreEqual(0, AES.Utils.SumGF256(128, 128));
+            Assert.AreEqual(0, AES.Utils.SumGF256(255, 255));
+
+            // main
+            Assert.AreEqual(0b00110011, AES.Utils.SumGF256(0b11110000, 0b11000011));
+            Assert.AreEqual(0b00101010, AES.Utils.SumGF256(0b11111111, 0b11010101));
+            Assert.AreEqual(0b11111001, AES.Utils.SumGF256(0b10100011, 0b01011010));
+        }
+
+        [TestMethod]
+        public void TestGCD()
+        {
+            // zeros
+            Assert.AreEqual(0, AES.Utils.GCD(0, 0));
+            Assert.AreEqual(1, AES.Utils.GCD(0, 1));
+            Assert.AreEqual(1, AES.Utils.GCD(1, 0));
+            Assert.AreEqual(byte.MaxValue, AES.Utils.GCD(byte.MaxValue, 0));
+            Assert.AreEqual(byte.MaxValue / 2, AES.Utils.GCD(byte.MaxValue / 2, 0));
+
+            // same
+            for (byte i = 1; i < byte.MaxValue; i += 1)
+                Assert.AreEqual(i, AES.Utils.GCD(i, i));
+
+            // main
+            Assert.AreEqual(4, AES.Utils.GCD(4, 60));
+            Assert.AreEqual(20, AES.Utils.GCD(40, 60));
+            Assert.AreEqual(5, AES.Utils.GCD(25, 255));
+            Assert.AreEqual(1, AES.Utils.GCD(251, 255));
+            Assert.AreEqual(1, AES.Utils.GCD(byte.MaxValue, 1));
+            Assert.AreEqual(64, AES.Utils.GCD(64, 128));
+            Assert.AreEqual(5, AES.Utils.GCD(25, 110));
+            Assert.AreEqual(1, AES.Utils.GCD(103, 207));
         }
     }
 }
