5a400c0cacff64f7f0244ddb518f55688aa08ebf
Nikolay <merzlovnik@mail.ru>
2023-05-07 14:27:17 +0300

    Sum in GF8

--- AES/Utils.cs
+++ AES/Utils.cs
@@ -2,5 +2,9 @@
 {
     public static class Utils
     {
+        public static byte SumGF8(byte lhs, byte rhs)
+        {
+            return (byte)(lhs ^ rhs);
+        }
     }
 }
--- AESTests/UtilsTests.cs
+++ AESTests/UtilsTests.cs
@@ -4,8 +4,12 @@ namespace AESTests
     public class UtilsTests
     {
         [TestMethod]
-        public void TestMethod()
+        public void TestSumGF8()
         {
+            Assert.AreEqual(0, AES.Utils.SumGF8(0, 0));
+            Assert.AreEqual(0, AES.Utils.SumGF8(1, 1));
+            Assert.AreEqual(0, AES.Utils.SumGF8(128, 128));
+            Assert.AreEqual(0, AES.Utils.SumGF8(255, 255));
         }
     }
 }
