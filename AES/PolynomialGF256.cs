6ccb9f886b4a61fa292769629826eef680b7bb00
Nikolay <merzlovnik@mail.ru>
2023-05-08 18:46:05 +0300

    PolynomialGF256 base functions

---
+++ AES/PolynomialGF256.cs
@@ -0,0 +1,170 @@
+﻿namespace AES
+{
+    /// <summary>
+    /// Allows you to perform the necessary operations on a finite Galois field 
+    /// constructed as an extension of the field GF(2) {0,1} modulo an irreducible polynomial
+    /// x^8 + x^4 + x^3 + x + 1.
+    /// </summary>
+    public class PolynomialGF256
+    {
+        // ------------------------------------------------------------------------------------------------------------
+        // Fields
+        // ------------------------------------------------------------------------------------------------------------
+        public uint Coefficients { get; private set; }
+
+        private static readonly int byteLength = 8;
+
+        private static readonly uint uintMaxLength = 32;
+
+        private static readonly uint mod = 283; // x^8 + x^4 + x^3 + x + 1
+
+        private static readonly uint higherBitMask = 1u << ((int)uintMaxLength - 1);
+
+        private static readonly PolynomialGF256 modPolynomial = new PolynomialGF256(mod);
+
+        // ------------------------------------------------------------------------------------------------------------
+        // Overloads
+        // ------------------------------------------------------------------------------------------------------------
+
+        public override string ToString()
+        {
+            var result = new List<string>();
+            uint mask = 1;
+            for (var i = 0; i < uintMaxLength; ++i)
+            {
+                if ((mask & Coefficients) != 0)
+                {
+                    result.Add(string.Format("x^{0}", i));
+                }
+                mask <<= 1;
+            }
+            if (result.Count == 0)
+                return "0";
+            result.Reverse();
+            return string.Join(" + ", result);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                PolynomialGF256 other = (PolynomialGF256)obj;
+                return Coefficients == other.Coefficients;
+            }
+        }
+
+        public static PolynomialGF256 operator <<(PolynomialGF256 arg, int shift)
+        {
+            arg.Coefficients <<= shift;
+            return arg;
+        }
+
+        public static PolynomialGF256 operator >>(PolynomialGF256 arg, int shift)
+        {
+            arg.Coefficients >>= shift;
+            return arg;
+        }
+
+        public static bool operator >(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            return lhs.Coefficients > rhs.Coefficients;
+        }
+
+        public static bool operator <(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            return lhs.Coefficients < rhs.Coefficients;
+        }
+
+        public static bool operator ==(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            return lhs.Coefficients == rhs.Coefficients;
+        }
+
+        public static bool operator !=(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            return lhs.Coefficients != rhs.Coefficients;
+        }
+
+        public static PolynomialGF256 operator +(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            return new PolynomialGF256(lhs.Coefficients ^ rhs.Coefficients);
+        }
+
+        public static PolynomialGF256 operator /(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            throw new Exception();
+        }
+
+        public static PolynomialGF256 operator %(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            if (rhs.IsZero())
+                throw new DivideByZeroException();
+            while (lhs.Length() >= rhs.Length())
+            {
+                PolynomialGF256 sub = new(rhs);
+                while (lhs.Length() > sub.Length())
+                {
+                    sub <<= 1;
+                }
+                lhs += sub;
+            }
+            if (lhs > modPolynomial)
+                lhs = ToMod(lhs);
+            return lhs;
+        }
+
+        public static PolynomialGF256 operator *(PolynomialGF256 lhs, PolynomialGF256 rhs)
+        {
+            throw new Exception();
+        }
+
+        // ------------------------------------------------------------------------------------------------------------
+        // Public
+        // ------------------------------------------------------------------------------------------------------------
+
+        public PolynomialGF256(uint coefficients)
+        {
+            Coefficients = coefficients;
+        }
+
+        public PolynomialGF256(PolynomialGF256 other)
+        {
+            Coefficients = other.Coefficients;
+        }
+
+        /// <summary>
+        /// Returns higher bit index
+        /// </summary>
+        public uint Length()
+        {
+            var mask = higherBitMask;
+            for (uint i = 0; i < uintMaxLength; ++i)
+            {
+                if ((mask & Coefficients) != 0)
+                    return uintMaxLength - i;
+                mask >>= 1;
+            }
+            return 0;
+        }
+
+
+
+        // ------------------------------------------------------------------------------------------------------------
+        // Private
+        // ------------------------------------------------------------------------------------------------------------
+
+        private static PolynomialGF256 ToMod(PolynomialGF256 arg)
+        {
+            return arg %= modPolynomial;
+        }
+
+        private bool IsZero()
+        {
+            return Coefficients == 0;
+        }
+    }
+}
---
+++ AESTests/PolynomialGF256.cs
@@ -0,0 +1,194 @@
+﻿using Polynomial = AES.PolynomialGF256;
+
+namespace AESTests
+{
+    [TestClass]
+    public class PolynomialGF256
+    {
+        [TestMethod]
+        public void Compare()
+        {
+            // equal
+            Assert.AreEqual(new Polynomial(1), new Polynomial(1));
+            Assert.AreEqual(new Polynomial(uint.MaxValue), new Polynomial(uint.MaxValue));
+            Assert.AreEqual(new Polynomial(0), new Polynomial(0));
+            Assert.AreEqual(new Polynomial(482397401u), new Polynomial(482397401u));
+
+            // not equal
+            Assert.AreNotEqual(new Polynomial(1), new Polynomial(0));
+            Assert.AreNotEqual(new Polynomial(uint.MaxValue), new Polynomial(7372));
+            Assert.AreNotEqual(new Polynomial(123), new Polynomial(456));
+            Assert.AreNotEqual(new Polynomial(482397401u), new Polynomial(482397400u));
+
+            // direct equal call
+            Assert.IsTrue(new Polynomial(1).Equals(new Polynomial(1)));
+            Assert.IsFalse(new Polynomial(1).Equals(new Polynomial(0)));
+            Assert.IsFalse(new Polynomial(0).Equals(1920));
+
+            // == !=
+            Assert.IsTrue(new Polynomial(1) == new Polynomial(1));
+            Assert.IsTrue(new Polynomial(uint.MaxValue) == new Polynomial(uint.MaxValue));
+            Assert.IsTrue(new Polynomial(0) == new Polynomial(0));
+            Assert.IsTrue(new Polynomial(482397401u) == new Polynomial(482397401u));
+            Assert.IsTrue(new Polynomial(1) != new Polynomial(0));
+            Assert.IsTrue(new Polynomial(uint.MaxValue) != new Polynomial(7372));
+            Assert.IsTrue(new Polynomial(123) != new Polynomial(456));
+            Assert.IsTrue(new Polynomial(482397401u) != new Polynomial(482397400u));
+
+            // > <
+            Assert.IsTrue(new Polynomial(1) > new Polynomial(0));
+            Assert.IsTrue(new Polynomial(1377) > new Polynomial(890));
+            Assert.IsTrue(new Polynomial(uint.MaxValue) > new Polynomial(uint.MaxValue - 1));
+            Assert.IsTrue(new Polynomial(26171) > new Polynomial(2921));
+            Assert.IsTrue(new Polynomial(0) < new Polynomial(1));
+            Assert.IsTrue(new Polynomial(376) < new Polynomial(38291));
+            Assert.IsTrue(new Polynomial(uint.MaxValue - 1) < new Polynomial(uint.MaxValue));
+            Assert.IsTrue(new Polynomial(371) < new Polynomial(372771));
+        }
+
+        [TestMethod]
+        public void Shifts()
+        {
+            // <<
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000000),
+                new Polynomial(0b00000000_00000000_00000000_00000000) << 2);
+
+            Assert.AreEqual(new Polynomial(0b11111100_00000000_00000000_00000000),
+                new Polynomial(0b11111111_00000000_00000000_00000000) << 2);
+
+            Assert.AreEqual(new Polynomial(0b11111111_11111111_11111111_11111111),
+                new Polynomial(0b11111111_11111111_11111111_11111111) << 32);
+
+            Assert.AreEqual(new Polynomial(0b00000000_11111111_00000000_00000000),
+                new Polynomial(0b11111111_00000000_11111111_00000000) << 8);
+
+            // >>
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000000),
+                new Polynomial(0b00000000_00000000_00000000_00000000) >> 2);
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00111111),
+                new Polynomial(0b00000000_00000000_00000000_11111111) >> 2);
+
+            Assert.AreEqual(new Polynomial(0b11111111_11111111_11111111_11111111),
+                new Polynomial(0b11111111_11111111_11111111_11111111) >> 32);
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_11111111_00000000),
+                new Polynomial(0b00000000_11111111_00000000_11111111) >> 8);
+        }
+
+        [TestMethod]
+        public void Sum()
+        {
+            // zeros
+            Assert.AreEqual(new Polynomial(0), new Polynomial(0) + new Polynomial(0));
+            Assert.AreEqual(new Polynomial(0), new Polynomial(1) + new Polynomial(1));
+            Assert.AreEqual(new Polynomial(0), new Polynomial(uint.MaxValue) + new Polynomial(uint.MaxValue));
+            Assert.AreEqual(new Polynomial(0), new Polynomial(25559) + new Polynomial(25559));
+
+            // main
+            Assert.AreEqual(new Polynomial(0b11111111_11111111_11111111_11111111),
+                new Polynomial(0b11111111_00000000_11111111_00000000) + new Polynomial(0b00000000_11111111_00000000_11111111));
+
+            Assert.AreEqual(new Polynomial(0b00000000_11111111_00000000_00000000),
+                new Polynomial(0b00000000_11111111_00000000_00000000) + new Polynomial(0b00000000_00000000_00000000_00000000));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_11111111_00000000),
+                new Polynomial(0b00000000_00000000_11111111_11111111) + new Polynomial(0b00000000_00000000_00000000_11111111));
+
+            Assert.AreEqual(new Polynomial(0b11111111_00000000_00000000_11111111),
+                new Polynomial(0b11111111_11111111_11111111_00000000) + new Polynomial(0b00000000_11111111_11111111_11111111));
+        }
+
+        [TestMethod]
+        public void ToStr()
+        {
+            Assert.AreEqual("0", new Polynomial(0).ToString());
+            Assert.AreEqual("x^0", new Polynomial(0b00000000_00000000_00000000_00000001).ToString());
+            Assert.AreEqual("x^31 + x^23 + x^15 + x^7", new Polynomial(0b10000000_10000000_10000000_10000000).ToString());
+            Assert.AreEqual("x^31 + x^23 + x^15 + x^7 + x^6 + x^5 + x^4 + x^3 + x^2 + x^1",
+                new Polynomial(0b10000000_10000000_10000000_11111110).ToString());
+        }
+
+        [TestMethod]
+        public void Length()
+        {
+            Assert.AreEqual(0u,
+                new Polynomial(0b00000000_00000000_00000000_00000000).Length());
+
+            Assert.AreEqual(32u,
+                new Polynomial(0b10000000_00000000_00000000_00000000).Length());
+
+            Assert.AreEqual(32u,
+                new Polynomial(0b11111111_00000000_00000000_00000000).Length());
+
+            Assert.AreEqual(24u,
+                new Polynomial(0b00000000_10000000_10000000_10000000).Length());
+
+            Assert.AreEqual(1u,
+                new Polynomial(0b00000000_00000000_00000000_00000001).Length());
+        }
+
+        [TestMethod]
+        public void DivisionReminder()
+        {
+            // zero reminder
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000000),
+                new Polynomial(0b00000000_00000000_00000000_00000000) %
+                new Polynomial(0b00000000_00000000_00000000_00000001));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000000),
+                new Polynomial(0b00000000_00000000_00000000_00000000) %
+                new Polynomial(0b10000000_10000000_10000000_00000001));
+            
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000000),
+                new Polynomial(0b00000000_00000000_00000000_00000001) %
+                new Polynomial(0b00000000_00000000_00000000_00000001));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000000),
+                new Polynomial(0b00000000_00000000_00000000_11111111) %
+                new Polynomial(0b00000000_00000000_00000000_11111111));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000000),
+                new Polynomial(0b00000000_00000000_00000000_10111001) %
+                new Polynomial(0b00000000_00000000_00000000_10111001));
+
+            // without ToMod()
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000001),
+                new Polynomial(0b00000000_00000000_00000000_00000001) %
+                new Polynomial(0b00000000_00000000_00000000_00000010));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00010000),
+                new Polynomial(0b00000000_00000000_00000000_10110000) %
+                new Polynomial(0b00000000_00000000_00000000_00110000));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00001101),
+                new Polynomial(0b00000000_00000000_00000000_10110110) %
+                new Polynomial(0b00000000_00000000_00000000_01101001));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00000001),
+                new Polynomial(0b10000000_00000000_00000000_00000000) %
+                new Polynomial(0b01111111_11111111_11111111_11111111));
+
+            // with ToMod() after division
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_00100101),
+                new Polynomial(0b00000000_00000000_10101110_10110110) %
+                new Polynomial(0b00000000_00000000_00100100_01101001));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_11111001),
+                new Polynomial(0b00000000_00000000_11111000_00000000) %
+                new Polynomial(0b00000000_00000000_00100001_11111111));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_11011000),
+                new Polynomial(0b00000000_00000000_00001000_00000000) %
+                new Polynomial(0b00000000_00000000_00100000_00000001));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_11000111),
+                new Polynomial(0b00000000_00000000_00111000_00000111) %
+                new Polynomial(0b00000000_00000000_00110000_00011000));
+
+            Assert.AreEqual(new Polynomial(0b00000000_00000000_00000000_10100111),
+                new Polynomial(0b00000000_00000000_11111000_00000111) %
+                new Polynomial(0b00000000_00000000_00110000_00011000));
+        }
+    }
+}
