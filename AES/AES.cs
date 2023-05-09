2b1819cfb06c0ba0414819cd67f2bdaa2e38c521
Nikolay <merzlovnik@mail.ru>
2023-05-09 22:29:46 +0300

    SBox generation

--- AES/AES.cs
+++ AES/AES.cs
@@ -2,5 +2,45 @@
 {
     public class AES
     {
+        // ------------------------------------------------------------------------------------------------------------
+        // Fields
+        // ------------------------------------------------------------------------------------------------------------
+
+        // ------------------------------------------------------------------------------------------------------------
+        // Public
+        // ------------------------------------------------------------------------------------------------------------
+        public static List<byte> GetForwardSBox(int seed)
+        {
+            List<byte> initial = new();
+            for (int i = 0; i <= byte.MaxValue; ++i)
+            {
+                initial.Add((byte)i);
+            }
+
+            List<byte> result = new();
+            var random = new Random(seed);
+            for (int i = 0; i <= byte.MaxValue; ++i)
+            {
+                var index = random.Next(initial.Count);
+                result.Add(initial[index]);
+                initial.RemoveAt(index);
+            }
+
+            return result;
+        }
+
+        public static List<byte> GetInverseSBox(List<byte> forwardSBox)
+        {
+            List<byte> result = new byte[byte.MaxValue + 1].ToList();
+            for (int i = 0; i <= byte.MaxValue; ++i)
+            {
+                result[forwardSBox[i]] = (byte)i;
+            }
+            return result;
+        }
+
+        // ------------------------------------------------------------------------------------------------------------
+        // Private
+        // ------------------------------------------------------------------------------------------------------------
     }
 }
--- AESTests/AESTests.cs
+++ AESTests/AESTests.cs
@@ -1,11 +1,47 @@
-﻿namespace AESTests
+﻿using AESS = AES.AES;
+
+namespace AESTests
 {
     [TestClass]
     public class AESTests
     {
-        [TestMethod]
-        public void TestMethod()
+
+        public void TestForwardSBox(int seed)
         {
+            var sBox = AESS.GetForwardSBox(seed);
+            Assert.AreEqual(byte.MaxValue + 1, sBox.Count);
+            for (int i = 0; i <= byte.MaxValue; ++i)
+            {
+                Assert.IsTrue(sBox.Contains((byte)i));
+            }
+        }
+
+        public void TestInverseSBox(int seed)
+        {
+            var forwardSBox = AESS.GetForwardSBox(seed);
+            var inverseSBox = AESS.GetInverseSBox(forwardSBox);
+            for (int i = 0; i <= byte.MaxValue; ++i)
+            {
+                Assert.IsTrue(forwardSBox.Contains((byte)i));
+                Assert.IsTrue(inverseSBox.Contains((byte)i));
+                Assert.AreEqual(i, inverseSBox[forwardSBox[i]]);
+            }
+        }
+
+        [TestMethod]
+        public void SBoxGeneration()
+        {
+            // forward
+            TestForwardSBox(0);
+            TestForwardSBox(int.MaxValue);
+            TestForwardSBox(36728);
+            TestForwardSBox(11111);
+
+            // inverse
+            TestInverseSBox(0);
+            TestInverseSBox(int.MaxValue);
+            TestInverseSBox(36728);
+            TestInverseSBox(11111);
         }
     }
 }
